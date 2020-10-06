733a7468931a4f85f33a12599c2b7cb45058fd8d
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-06 02:07:42 -0700

    feat: use gql tools, but there is a bug

--- projects/server/src/index.ts
+++ projects/server/src/index.ts
@@ -1,21 +1,6 @@
 import { ApolloServer, gql } from "apollo-server"
-
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+import { loadSchema } from "@graphql-tools/load"
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader"
 
 const books = [
     {
@@ -28,20 +13,22 @@ const books = [
     },
 ];
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-    Query: {
-        books: () => books,
-    },
+const startServer = async () => {
+    const typeDefs = await loadSchema('./src/schema/*.graphql', {
+        loaders: [
+            new GraphQLFileLoader()
+        ]
+    });
+    const resolvers = {
+        Query: {
+            books: () => books,
+        },
+    };
+    const server = new ApolloServer({ typeDefs, resolvers });
+    // The `listen` method launches a web server.
+    server.listen().then(({ url }) => {
+        console.log(`🚀  Server ready at ${url}`);
+    });
 };
 
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
-
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+startServer();
